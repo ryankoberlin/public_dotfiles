dbccb9695dada90030833c5f166becf90e4f6aad
Ryan Oberlin <roberlin@rona.ryanoberl.in>
2022-06-15 11:02:09 -0400

    Updates/general cleanup & sanitization

--- .functions.bash
+++ .functions.bash
@@ -1,87 +1,5 @@
 ### Functions for bashrc ###
 
-#: bsdrd @ backs up current kernel, and installs new @ bsdrd [-f file]
-function bsdrd() {
-    RD_URL="https://ftp.openbsd.org/pub/OpenBSD/snapshots/amd64/bsd.rd"
-    if $(! test -t 0); then # Log output if we're not running interactively
-        exec 3>&1 4>&2 >>~/.update.log 2>&1
-    fi
-    case $1 in 
-        -f)
-            if [[ ${#@} -eq 2 ]]; then
-            RD=$2
-        else
-            return 1
-        fi
-        ;;
-        -h)
-        echo "bsdrd [-f file] -d"
-        return 0;
-        ;;
-        -d)
-        echo "Downloading from ${RD_URL}"
-        pushd ~/Downloads
-        wget 2>&1>/dev/null $RD_URL
-        popd
-        ;;
-        *)
-        echo "bsdrd [-f file] [-d]"
-        return 0
-        ;;
-    esac
-    echo "Backing up..."
-    doas mv -v /bsd.rd{,-$(date "+%Y-%m-%d")}
-    if [[ -f $RD ]]; then
-        echo "Using $RD"
-        doas mv -v $RD /bsd.rd
-        touch ~/.update
-    else
-        echo "Using default bsd.rd in ~/Downloads"
-        doas mv -v /home/roberlin/Downloads/bsd.rd /bsd.rd
-        touch ~/.update
-    fi
-    _portsupdate
-    exec 1>&3 2>&4
-
-}
-
-#: _portsupdate @ Updates ports tree after reboot @ NULL
-function _portsupdate() {
-    _pushd () { pushd 2>&1>/dev/null $1; }
-    _popd () { popd 2>&1>/dev/null $1; }
-    CWD=$PWD
-    PORT_URL="https://cdn.openbsd.org/pub/OpenBSD/snapshots/ports.tar.gz"
-    SIG_URL="https://cdn.openbsd.org/pub/OpenBSD/snapshots/amd64/SHA256.sig"
-    if [[ ! -f ~/.updated ]]; then
-        echo "No update to perform, exiting"
-        return 1
-    fi
-    echo "Matching ports tree"
-    _pushd /tmp
-    ftp $PORT_URL
-    ftp $SIG_URL
-#    popd 2>&1>/dev/null
-# We don't have a shasum on -current
-#    echo "Running $(signify -Cp /etc/signify/openbsd-$(uname -r | cut -c 1,3)-base.pub -x SHA256.sig ports.tar.gz)"
-#    if $(signify -Cp /etc/signify/openbsd-$(uname -r | cut -c 1,3)-base.pub -x SHA256.sig ports.tar.gz); then
-#        pushd 2>&1>/dev/null /usr
-        echo "Password required:"
-        doas echo "Updating"
-        _pushd /usr
-        if $(doas tar -xzf /tmp/ports.tar.gz); then
-            rm -f ~/.updated
-            _popd
-        else
-            echo "tar failed"
-            return 1
-        fi
-        _pushd $CWD
-#    else
-#        echo "Signature does not match"
-#        return 1
-#    fi
-}
-
 #: lsd @ Lists $DIRSTACK array content @ <lsd> or <lsd -c> to clear stack
 function lsd() {
 	if [[ $1 == "-c" ]]
@@ -91,6 +9,11 @@ function lsd() {
 		else
 	dirs -l -v | head -11
 	fi
+}
+
+#: checkcert @ Leverage OpenSSL to examine cert details @ checkcert www.google.com
+function checkcert() {
+	openssl s_client -showcerts -connect ${1}:443 | ${PAGER:-less}
 }
 
 #: lsc @ Quickly switch between directories in the stack @ lsc $NUMBER
@@ -106,26 +29,26 @@ function lsc() {
 
 #: cd @ Adds directories to the stack and changes to them @ cd $DIR
 function cd() {
-	if [[ ${#@} < 1 ]]
-		then
-		if [[ -d /home/$(whoami) ]]; then
-			pushd /home/$(whoami) 1>/dev/null
-				else
-			pushd ~/ 1>/dev/null
-		fi
-		else
-		if [[ $1 == '-' ]]; then
-			pushd 1>/dev/null "$OLDPWD"
-		else
-			if [[ -d "$1" ]]; then
-				pushd "$1" 1>/dev/null
-			else
-				echo -e "${LIGHTRED}No such file or directory: ${1}${RESET}";
-				return 1;
-			fi
-		fi
-	fi
-	echo -e "${BLUE}> $PWD${RESET}"
+        if [[ ${#@} -lt 1 ]]
+                then
+                if [[ -d /home/$(whoami) ]]; then
+                        pushd /home/"$(whoami)" 1>/dev/null || return 2
+                                else
+                        pushd ~/ 1>/dev/null || exit
+                fi
+                else
+                if [[ $1 == '-' ]]; then
+                        pushd 1>/dev/null "$OLDPWD" || return 2
+                else
+                        if [[ -d $1 ]]; then
+                                pushd "$1" 1>/dev/null || return 2
+                        else
+                                echo -e "${LIGHTRED}No such file or directory: ${1}${RESET}";
+                                return 1;
+                        fi
+                fi
+        fi
+        echo -e "${BLUE}> $PWD${RESET}"
 }
 
 #: fnls @ List functions @ fnls $FUNCTION
@@ -138,20 +61,52 @@ l l l.
 CMD @ Description @ Usage
 .TH
 .SP
-$(grep ^#: ~/.functions.bash | cut -c4-|sort -nk1)
+$(grep ^#:\ ${1} ~/.functions.bash | cut -c4-|sort -nk1)
 .TE
 "|tbl |nroff 2>/dev/null -ms -Tascii|/usr/bin/less -Fdcs
 ##-Tascii
 }
 
+#: p5lib @ Sets PERL5LIB to /home/$(whoami)/runjob/lib @ p5lib [-u]
+function p5lib() {
+	case $1 in
+		-unset|-u)
+		export PERL5LIB=''
+		;;
+		-display|-d)
+		echo $PERL5LIB
+		;;
+		-set|-s)
+		export PERL5LIB=/home/$(whoami)/cvs/runjob/lib
+		;;
+		*)
+		echo "Usage: p5lib -s[et] -d[isplay] -u[nset]"
+		;;
+	esac
+}
+
 #: fnhelp @ Show help for functions @ fnhelp $FUNCTION
 function fnhelp() {
-	awk "/^#: ${1}/, /^$/" ~/.functions.bash|less
+	if [[ -e /home/$(whoami)/git/Scripts/${1} ]]; then
+		view $(which ${1})
+	else	
+		awk "/^#: ${1}/, /^$/" ~/.functions.bash|less
+	fi
 }
 
 #: fnsearch @ Search functions for term @ fnsearch $TERM
 function fnsearch() {
 	grep -iE ^\#\:.*${1} ~/.functions.bash|awk -F': ' '{print $2}'|less
+}
+
+#: lmstat @ License checking @ lmstat $LICSERVER
+function lmstat {
+	/apps/ansys/v140/fluent/bin/lmstat -a -c $1
+}
+
+#: findapp @ Locate app within /apps/ dir @ findapp $APP
+function findapp () { 
+	find /apps/ -maxdepth 2 -type d -iname \*$1\* 2>&1 |grep -v Permission\ denied
 }
 
 #: findso @ Locate SO file within /usr/ @ findso libxml.so
@@ -163,11 +118,6 @@ function findso() {
 function findext() {
 	find $PWD -type f -iname \*$1
 }
-
-#: epoch @ Convert UNIX epoch time to local time @ epoch 123456789
-function epoch() { 
-	date -d @"$1"; 
-}  
 
 #: lest @ Automatically view most recently edited file in cwd @ lest
 function lest() { 
@@ -188,6 +138,66 @@ function mypath() {
 	echo $PATH|tr ':' '\n'|sort -nbk 1,2|uniq
 }
 
+#: nodestat @ Display basic information about the current node @ nodestat
+function nodestat() {
+mem_total=$(free -g|awk '/Mem:/{print $2}')
+mem_used=$(free -g|awk '/Mem:/{print $3}')
+mem_free=$(($mem_total - $mem_used))
+ncpus=$(nproc)
+cpu_type=$(grep model\ name /proc/cpuinfo|cut -d' ' -f4-|head -n1)
+ip=$(ip a 2>/dev/null |awk '/inet\ 19\./{print $2, $NF}')
+if [[ -f /etc/SuSE-release ]]; then
+	os=$(grep SUSE /etc/SuSE-release)
+	else
+	os=$(cat /etc/redhat-release)
+fi
+kernel=$(uname -r)
+load=$(awk '{print $1, $2, $3}' /proc/loadavg)
+jobs=$(fqmgr -n $(hostname)|grep jobs  |tr ',' '\n'|cut -d\/ -f1|awk '{print $NF}'|uniq -c|awk '{print $1"*"$2}')
+building=$(cat /etc/conf/BUILDING)
+printf $LIGHTGREEN
+printf "
+.TS
+allbox, tab(*), center;
+cB s.
+Nodestat
+.T&
+cB s.
+::General::
+.T&
+l l.
+Node:*$HOSTNAME
+IP:*$ip
+LOC:*$building
+OS:*$os
+Kernel:*$kernel
+Load:*$load
+.T&
+cB s.
+::CPU::
+.T&
+l l.
+Cores:*$ncpus
+Type:*$cpu_type
+.T&
+cB s.
+::Memory::
+.T&
+l l.
+Total:*$mem_total Gb
+Used:*$mem_used Gb 
+Free:*$mem_free Gb 
+.T&
+cB s.
+::Jobs Running::
+.T&
+l l.
+CPUs*Job ID
+$jobs
+.TE
+"|tbl|nroff|more -sp
+printf $WHITE
+}
 
 #: lse @ FTofil - lists entries that match the starname(s) @ lse *
 function lse() { 
@@ -206,10 +216,9 @@ function lsvnc() {
 #: wi @ FTofil - lists the logged in users sorted by idle time @ wi
 function wi() { 
 	( who -Hu | head -n 1 | sed -e 's/IDLE /  IDLE/';
-	who -u | sed -e '/^\(.\{38\} \)  .  /s//\1 0.00/' \
- 	-e '/^\(.\{38\} \) old /s//\124.00/' -e 's/^\(.\{38\} \)/\1 /' -e 's/ (/  (/' |
+	who -u | sed -e '/^\(.\{38\} \)  .  /home//\1 0.00/' \
+ 	-e '/^\(.\{38\} \) old /home//\124.00/' -e 's/^\(.\{38\} \)/\1 /' -e 's/ (/  (/' |
   	sort -b -k 5.1,5.5 -k 1,1 -k 3,3 -k 4,4 -k 6n,6 ) | ${PAGER:-less};
-
 }
 
 #: uc @ Convert lower case to upper @ uc TEXT
@@ -240,7 +249,6 @@ function als() {
 #: halp @ Displays basic help info about functions @ halp
 function halp() {
 perl -se '
-
 ## DO NOT USE TABS IN THIS ARRAY 
 my @text = (
 "EDITOR.......$ENV{EDITOR}",
@@ -275,6 +283,15 @@ my @text = (
 " ",
 "Vim notes:",
 "s/\\s*\$//g....Remove trailing whitespace",
+"dib..........Delete block between braces",
+"dtX..........Delete until symbol 'X'",
+"daX..........Delete until (and) symbol 'X'",
+"zf{action}...Create fold",
+"zfa{symbol}..Create fold until next match of {symbol}",
+"za...........Toggle fold",
+"zd...........Delete fold",
+"zR...........Unfold all folds",
+"zM...........Fold all folds",
 " ",
 "Awk:",
 "FS...........Input Field Separator",
@@ -284,14 +301,24 @@ my @text = (
 "ORS..........Output Record Separator",
 "FILENAME.....Filename",
 );
-	
 print "+------+\n| HALP |\n+------+", "-"x77, "+\n";
-
-
 foreach (@text) {
 	print "|", pack("A84", $_), "|\n";
 }
 print "+", "-"x84,"+\n"; '
+}
+
+#: view @ Open a file in Vim (RO) @ view ~/.bashrc
+function view() { 
+if [[ ${#@} -ne 0 ]]; then
+	if [[ -e $1 ]]; then
+		vim -nM -c "set nonumber" -c "set foldcolumn=0" -c "set foldmethod=manual" -c "noremap q :exit <CR>" $1
+	else 
+		echo "No such file: $1"
+	fi
+else
+	return 1
+fi
 }
 
 #: meeseeks @ monitors PID and rings bell when done @ meeseeks $prog
@@ -315,7 +342,12 @@ who -u | sort -b -k 1,1 -k 3,3 -k 4,4 -k 6n,6 ) | ${PAGER:-less}
 
 #: lsdir @ List directories in CWD @ lsdir
 function lsdir() {
-	ls -lahd */ | sed 's|/$||g'
+	if [[ $1 ]]; then
+		dir=$1
+	else 
+		dir="."
+	fi
+	find $dir -maxdepth 1 -type d|sed 's|\./||g;'| sed 1d|sort
 }
 
 #: reload @ Reload bashrc and functions @ reload
@@ -348,6 +380,25 @@ function mkcd() {
 		mkdir $1
 		cd $1
 	fi
+}
+
+#: lsmyvnc @ show all of my VNC sessions on all login nodes @ lsmyvnc
+function lsmyvnc() {
+
+	ME=$(whoami)
+
+	login_nodes=" "
+	for PID in $(ls /home/${ME}/.vnc/*.pid|cut -d\: -f1|uniq); 
+	do 
+	login_nodes+="$(basename $PID) "
+	done
+
+    echo "VNC Sessions on all login servers:";
+    for i in $login_nodes;
+    do
+        printf "${i}:\n";
+        ls /home/${ME}/.vnc/${i}*.pid 2>&1 | grep -Po "(?<=\:).*(?=\.)" | grep -v cannot\ access;
+    done
 }
 
 #: colors @ list all available colors with examples @ colors
@@ -393,7 +444,179 @@ function unbinary() {
 	' -- "$@"
 }
 
-#: rename @ Removes youtube-dl tags @ rename
-function rename() {
-    ls -1 *.flac|while read f; do name=$(echo $f | sed 's/\-.*.flac/.flac/g'); mv "$f" "$name"; done
+#: coltest @ test number of columns because Cygwin is dumb @ coltest
+function coltest() {
+	perl -e '
+	print qq(Columns reported: \001\033[1;32m\002$ENV{COLUMNS}\001\033[0;00m\002\nLines should not exceed terminal width\n);
+	print "-" x $ENV{COLUMNS};
+	print "\n";
+'
 }
+
+#: qlist @ Query fqstat and parse number of jobs in states Q, D, R @ qlist
+function qlist() {
+dc=0 
+qc=0
+rc=0 
+if [[ ${#@} > 1 ]]; then
+	echo "qlist function takes no arguments"
+	return 127
+fi
+while read LINE; 
+	do 
+		case $LINE in
+		   Q) (( qc+=1 )) ;;
+		   D) (( dc+=1 )) ;;
+		   R) (( rc+=1 )) ;;
+		esac
+	done < <(fqstat -u all | awk '{if ($5 ~ "(Q|R|D)") { print $5 } }')
+echo -ne "Running:\t$rc\nQueued:\t\t$qc\nDep:\t\t$dc\n\n";
+}
+
+#: compare @ compare file across hosts @ compare [HOST] [FILE]
+function compare() {
+ID=$(whoami)
+FILE=$(realpath $2)
+RHOST=$1
+RFILE=$(ssh -q ${ID}@${RHOST} md5sum ${FILE}|awk '{print $1}')
+LFILE=$(md5sum ${FILE}|awk '{print $1}')
+if [[ ${#@} != 2 ]]; then
+        printf "Usage: $0 [FILE] [Remote Host]\n"
+        exit
+fi
+if [[ ! -e $FILE ]]; then
+        printf "ERR: $FILE not found\n"
+        exit
+    else
+        printf "\nComparing $FILE on ${YELLOW}${HOSTNAME}${RESET} and ${YELLOW}${RHOST}${YELLOW}\n\n"
+        if [[ $RFILE == $LFILE ]]; then
+                printf "${YELLOW}Local file checksum:\n${LFILE}\n${RESET}"
+                printf "${LIGHTGREEN}Remote file checksum:\n${RFILE}\n${RESET}\n"
+        else
+                printf "${YELLOW}Local file checksum:\n${LFILE}\n${RESET}"
+                printf "${LIGHTRED}Remote file checksum:\n${RFILE}\n${RESET}\n"
+        fi
+fi
+}
+
+#: cpustat @ Show core usage for all jobs @ cpustat
+function cpustat() {
+	nodes=$(fqstat |awk '{if ($4 ~ /[0-9]{1,}/) { print $4 } }')
+	num_jobs=$(echo "$nodes" | wc -l)
+	num_cpus=$(echo "$nodes" | paste -sd+ | bc)
+	echo -e "$num_jobs jobs\n${num_cpus} cores"
+}
+
+#: getumask @ Looks up umask for user from LDAP @ getumask $username
+function getumask() {
+	test ${#@} == 1 || return 1
+	ldapsearch -xLLL uid=${1} gecos | grep -Eo 'umask=[0-7]{3}'
+}
+
+#: fld @ Add lines and fold text file @ fld $FILE
+function fld() {
+	FILE="$1"
+	OUTFILE=$(basename ${FILE}.fld)
+	if [[ ! -f "$FILE" ]]; then
+		echo "$FILE not found!"
+		return 1
+	fi
+	nl -ba "$FILE" |fold -sc100 >> /home/$(whoami)/Documents/Print/${OUTFILE}
+}
+
+#: sehelp @ Show SELinux help notes @ sehelp
+function sehelp() {
+	echo "
+	ausearch -m avc -ts today 	# View denials for today
+	ausearch -m avc 			# View all denials
+	ausearch -m avc -c httpd 	# View denails for specific comm
+	
+	aureport -a					# Produces summary report of all audit system logs
+	
+	restorecon -R -v $DIR/		# Restore context of directory (recursive)
+	"
+}
+
+#: bup @ Backup file @ bup FILE
+function bup() {
+	declare -a FILES
+	FILES=${@}
+	TIME=$(date +%H%M.%m%d%y)
+	for f in ${FILES[*]}; 
+		do mv "$f" "${f}.bup.${TIME}"
+	done
+	}
+
+#: killnast @ Kill all running nastran processes @ killnast
+function killnast() {
+	ps faux|grep -i [n]astran|awk '{print $2}'| \
+	while read PID; 
+		do 
+			kill -9 $PID
+		done
+}
+
+#: deploy @ Create run directories for Nastran testing @ create $RUN $ZIP
+function deploy() {
+	if [[ ${#@} != 1 ]]; then
+		echo "deploy takes one argument"
+		return 1
+	fi
+	ZIP=$1
+	ZIPDIR=/home/$(whoami)/models/NASTRAN/tars
+	RUNDIR=/home/$(whoami)/models/NASTRAN/NUMA
+	OLD=$(basename $(ls -1vd ${RUNDIR}/run* | tail -n1) | cut -c4-)
+	NEW=$(( $OLD+1 ))
+	RUN=run${NEW}
+	mkdir -p ${RUNDIR}/${RUN}/{0..3}
+	echo "${RUNDIR}/${RUN}:"
+	for i in ${RUNDIR}/${RUN}/{0..3};
+		do pushd 2>&1 > /dev/null $i;
+		echo "${NEW},$(basename $i)"
+			tar -xzvf ${ZIPDIR}/${ZIP} 1>/dev/null;
+		popd 2>&1 > /dev/null;
+	done;
+}
+
+#: _deploy @ Tab completion for deploy() @ NULL
+function _deploy() {
+    local cur
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    local zips=$(for f in $(ls -1 /home/$(whoami)/models/NASTRAN/tars/*.tar.gz); do echo $(basename ${f}); done)
+    COMPREPLY=($(compgen -W "${zips}" $cur))
+}
+
+#: cdl @ cd to most recently created directory @ cdl
+function cdl() {
+	DIR=$(ls -rtd1 ${PWD}/*|tail -n1)
+	if [ -d $DIR ]; then
+		cd $DIR
+	else 
+		echo "$DIR does not exist"
+		return 1
+	fi
+}
+
+#: psgrep @ Quickly search the ps tree for match @ psgrep -m nastran
+function psgrep() {
+	echo 'USER       PID STAT  START COMMAND'
+	echo '----------------------------------'
+	if [[ $1 == -m ]]; then
+		ps -u $(whoami) -ouser,pid,stat,bsdstart,command --sort stat | grep --color=never -i $(echo $2|sed 's/^./\[&\]/g')
+	else
+		ps ax -ouser,pid,stat,bsdstart,command --sort stat| grep --color=never -i $(echo $1|sed 's/^./\[&\]/g')
+	fi
+}
+
+#: vtmp @ Edit text quickly in a volitile file in vanilla(ish) vim @ vtmp
+function vtmp() {
+	if [[ ${#@} > 1 ]]; then
+	 	echo "vtmp takes no arguments"
+		return 1
+	fi
+	T_FILE=$(mktemp --tmpdir=/home/$(whoami)/tmp)
+	vim -n -c "set nonumber" -c "set foldcolumn=0" -c "set foldmethod=manual"  $T_FILE
+	rm -f $T_FILE
+}
+
